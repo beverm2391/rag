742b09ad1fa60cb6948d1a294a5e08433af5711b
Ben Everman <94769250+beverm2391@users.noreply.github.com>
2024-04-02 17:19:05 -0400

    update documentation

--- README.md
+++ README.md
@@ -1,10 +1,40 @@
-# My Retrieval Augmented Generation (RAG) Implementation
+# My Retrieval Augmented Generation (RAG) Implementations
+
+## Goals
+
+The first goal of this project is to build a robust RAG system that encapsulates the newest methods and models in an intuitive python interface. The second goal is to put all this logic on a server and expose REST APIs to a Next.js frontend that will include a Generative User Intergace. TBD
+
+## Core 
+
+This is a work in progress. Right now I'm finishing up the core abstractions. Everything is broken down into a few core services:
+
+- `data.py`
+  - this includes the `Index` class (embedding database)
+  - and the `Reranker` class
+- `generators.py`
+  - this containes the generators that use [instructor]() to output schema-validated data
+  - `models.py` contains the data models
+- `chat.py`
+  - this class wraps around popular LLM APIs and exposes certain methods for interface
+- `rag.py`
+  - TODO: this class will have an end to end configurable rag solution and leverage each of the aforementioned abstractions
+- `utils.py`
+  - pretty self explanatory, just utils and configuration
 
 
-## Completer
+
+
+## Completed
 - [X] Update the generator code to use Anthropic
 - [X] add reranking to the index class
 - [X] Implement the multiple query inside the index by searching all queries, then removing duplicates, before finally reranking
 
 ## TODO
-- [ ] test the ability to generate queries that include the best location/db to search for
+- [ ] finish the Cohere class in `chat.py`
+- [ ] finish the Chat factory class
+- [ ] sanity check all the new abstractions
+- [ ] figure out how you want to test this repo - pytest probably
+- [ ] build and text the ability to generate queries that include the best location/db to search for
+- [ ] Add tool calls for openai and Anthropic
+- [ ] add Instructor support for Chat models
+- [ ] move the models object from `utils.py` and to a .json config file 
--- lib/chat.py
+++ lib/chat.py
@@ -3,6 +3,7 @@ from typing import Generator
 from openai import OpenAI
 from anthropic import Anthropic
 import cohere
+from cohere.responses.chat import StreamEvent
 import instructor
 
 from lib.utils import MODELS, load_env
@@ -150,14 +151,73 @@ class AnthropicChat(ChatABC):
             self.messages.append(new_message)
 
 class CohereChat(ChatABC):
-    def __init__(self, model: str):
-        super().__init__(model)
+    """Chat with Cohere"""
+    _name = "cohere"
+    _api_key = COHERE_API_KEY
+
+    def __init__(
+            self, model_name: str, temperature: float, max_tokens: int, system_prompt: str, debug: bool = False,
+            model_var: str = None, context_window: int = 0, web_search: bool = False, citations: bool = False):
+        super().__init__(model_name)
+        self._init_client()
+        self.temperature = temperature
+        self.max_tokens = max_tokens
+        self.system_prompt = system_prompt
+        self.model_var = model_var | model_name
+        self.context_window = context_window
+        self.web_search = web_search
+        self.citations = citations
+
+        self.messages = [
+            {"role": "SYSTEM", "message": system_prompt},
+        ]
+
+        self.debug = debug
+        
+    def _init_client(self):
+        self.client = cohere.Client(api_key=COHERE_API_KEY)
+        if self.debug: print(f"Initialized {self._name} client.")
 
     def chat(self, text: str) -> str:
-        pass
+        res = self.client.chat(
+            model=self.model_var,
+            chat_history=self.messages,
+            message={"role": "USER", "message": text},
+            temperature=self.temperature,
+            max_tokens=self.max_tokens,
+            connectors=[{"id": "web-search"}] if self.web_search else None,
+        )
+        self.messages.append({"role": "CHATBOT", "message": res['text']})
+        return {
+            "text" : self.messages[-1]['message'],
+            "search_queries" : res.get("search_queries", None),
+            "search_results" : res.get("search_results", None),
+            "tool_calls" : res.get("tool_calls", None),
+            "citations" : res.get("citations", None),
+        }
 
     def chat_stream(self, text: str) -> Generator[str, None, None]:
-        pass
+        res_stream = self.client.chat(
+            model=self.model_var,
+            chat_history=self.messages,
+            message={"role": "USER", "message": text},
+            temperature=self.temperature,
+            max_tokens=self.max_tokens,
+            connectors=[{"id": "web-search"}] if self.web_search else None,
+            stream=True,
+        )
+        new_message = {"role": "CHATBOT", "message": ""}
+        for event in res_stream:
+            if event.event_type == StreamEvent.TEXT_GENERATION:
+                new_message["message"] += event.text
+                yield event.text
+            elif event.event_type == StreamEvent.STREAM_END:
+                # this is where you parse the citations, search results, etc.
+                # TODO: https://docs.cohere.com/docs/streaming#using-streaming
+                pass
+
+        self.messages.append(new_message)
+
 
 class InstructorOpenAIChat(ChatABC):
     pass
@@ -170,6 +230,7 @@ class InstructorAnthropicChat(ChatABC):
 class Chat:
     _models = MODELS
 
+    # TODO: make sure to pass in the correct parameters for each model from the MODELS object
     def __new__(cls, model: str):
         if model not in cls._models: raise ValueError(f"Model {model} not in {cls._models}")
         if model == "openai": return OpenAIChat(model)
