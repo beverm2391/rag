7c60e7362613e175faefa70a2a26817ea0d735c0
Ben Everman <94769250+beverm2391@users.noreply.github.com>
2024-04-28 15:28:31 -0400

    tests are passing agian

--- tests/lib/test_chat_external.py
+++ tests/lib/test_chat_external.py
@@ -14,14 +14,18 @@ def example_args():
     return [temperature, max_tokens, system_prompt]
 
 @pytest.fixture
+def example_kwargs():
+    return {"persist" : True}
+
+@pytest.fixture
 def example_prompts(): return "What is the meaning of life?", "Why is the sky blue?",
 
 
 @pytest.mark.external
-def test_chat_openai(example_args, example_prompts):
+def test_chat_openai(example_args, example_prompts, example_kwargs):
     example_prompt_1, example_prompt_2 = example_prompts
 
-    chat = Chat.create("gpt-3.5-turbo", *example_args, persist=True, debug=True) # persist true because we check number of messages later
+    chat = Chat.create("gpt-3.5-turbo", *example_args, **example_kwargs) # persist true because we check number of messages later
     res = chat.chat(example_prompt_1)
     text = res['data']['text']
 
@@ -37,10 +41,10 @@ def test_chat_openai(example_args, example_prompts):
     assert (l:=len(chat.messages)) == 5, f"Chat messages should be 5 (system, user, chatbot, user, chatbot), is {l}"
 
 @pytest.mark.external
-def test_chat_anthropic(example_args, example_prompts):
+def test_chat_anthropic(example_args, example_prompts, example_kwargs):
     example_prompt_1, example_prompt_2 = example_prompts
 
-    chat = Chat.create("claude-3-haiku", *example_args)
+    chat = Chat.create("claude-3-haiku", *example_args, **example_kwargs)
     res = chat.chat(example_prompt_1)
     text = res['data']['text']
 
@@ -57,10 +61,10 @@ def test_chat_anthropic(example_args, example_prompts):
 
 
 @pytest.mark.external
-def test_chat_cohere(example_args, example_prompts):
+def test_chat_cohere(example_args, example_prompts, example_kwargs):
     example_prompt_1, example_prompt_2 = example_prompts
 
-    chat = Chat.create("command-r", *example_args)
+    chat = Chat.create("command-r", *example_args, **example_kwargs)
     res = chat.chat(example_prompt_1)
     text = res['data']['text']
 
@@ -79,8 +83,8 @@ def test_chat_cohere(example_args, example_prompts):
 # ! Stream =================================================
 
 @pytest.mark.external
-def test_chat_openai_stream(example_args, example_prompts):
-    chat = Chat.create("gpt-3.5-turbo", *example_args)
+def test_chat_openai_stream(example_args, example_prompts, example_kwargs):
+    chat = Chat.create("gpt-3.5-turbo", *example_args, **example_kwargs)
     res = chat.chat_stream(example_prompts[0])
     validate_stream(res)
 
@@ -89,8 +93,8 @@ def test_chat_openai_stream(example_args, example_prompts):
     assert (l:=len(chat.messages)) == 5, f"Chat messages should be 5 (system, user, chatbot, user, chatbot), is {l}"
 
 @pytest.mark.external
-def test_chat_anthropic_stream(example_args, example_prompts):
-    chat = Chat.create("claude-3-haiku", *example_args)
+def test_chat_anthropic_stream(example_args, example_prompts, example_kwargs):
+    chat = Chat.create("claude-3-haiku", *example_args, **example_kwargs)
     res = chat.chat_stream(example_prompts[0])
     validate_stream(res)
 
@@ -100,9 +104,9 @@ def test_chat_anthropic_stream(example_args, example_prompts):
     assert (l:=len(chat.messages)) == 4, f"Chat messages should be 4 (user, chatbot, user, chatbot), is {l}" # Claude doesn't have a system message
 
 @pytest.mark.external
-def test_chat_cohere_stream(example_args, example_prompts):
+def test_chat_cohere_stream(example_args, example_prompts, example_kwargs):
     example_args = [example_args[0], 1000, example_args[2]] # ? The command-light model has a context_window of 4096 so we need to reduce the max_tokens
-    chat = Chat.create("command-light", *example_args)
+    chat = Chat.create("command-light", *example_args, **example_kwargs)
     res = chat.chat_stream(example_prompts[0])
     validate_stream(res)
 
